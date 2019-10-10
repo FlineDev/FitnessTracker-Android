3f242879a8b6dda9c4b1fb02b539f1d8c9584dd4
Cihat Gündüz <commits@cihatguenduez.de>
2019-10-10 07:55:30 +0200

    Add icons to bottom navigation bar

--- app/src/main/java/com/flinesoft/fitnesstracker/MainActivity.kt
+++ app/src/main/java/com/flinesoft/fitnesstracker/MainActivity.kt
@@ -2,11 +2,16 @@ package com.flinesoft.fitnesstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navCtrl = findNavController(R.id.navHostFragment)
+        bottomNavigationView.setupWithNavController(navCtrl)
     }
 }
