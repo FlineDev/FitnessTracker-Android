f5cac134ea40b835a680f071b0e73845e41d5bf0
Cihat Gündüz <commits@cihatguenduez.de>
2019-11-28 04:51:59 +0100

    Add measure formatting tests & fix issue with big numbers

---
+++ app/src/androidTest/java/com/flinesoft/fitnesstracker/globals/extensions/MeasureFormatExtensionsKtTest.kt
@@ -0,0 +1,36 @@
+package com.flinesoft.fitnesstracker.globals.extensions
+
+import android.icu.text.MeasureFormat
+import android.icu.util.MeasureUnit
+import android.icu.util.ULocale
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MeasureFormatExtensionsKtTest {
+    private val measureFormat = MeasureFormat.getInstance(ULocale.US, MeasureFormat.FormatWidth.SHORT)
+
+    @Test
+    fun valueToStringForDouble() {
+        assertEquals("42.4 kg", measureFormat.valueToString(42.4, MeasureUnit.KILOGRAM))
+        assertEquals("1,000,000.001 cm", measureFormat.valueToString(1_000_000.001, MeasureUnit.CENTIMETER))
+    }
+
+    @Test
+    fun valueToStringForInt() {
+        assertEquals("42 kg", measureFormat.valueToString(42, MeasureUnit.KILOGRAM))
+        assertEquals("1,000,000 cm", measureFormat.valueToString(1_000_000, MeasureUnit.CENTIMETER))
+    }
+
+    @Test
+    fun stringToDouble() {
+        assertEquals(42.4, measureFormat.stringToDouble("42.4 kg", MeasureUnit.KILOGRAM))
+        assertEquals(1_000_000.001, measureFormat.stringToDouble("1,000,000.001 cm", MeasureUnit.CENTIMETER))
+    }
+
+    @Test
+    fun stringToInt() {
+        assertEquals(42, measureFormat.stringToInt("42 kg", MeasureUnit.KILOGRAM))
+        assertEquals(1_000_000, measureFormat.stringToInt("1,000,000 cm", MeasureUnit.CENTIMETER))
+    }
+}
--- app/src/main/java/com/flinesoft/fitnesstracker/globals/extensions/MeasureFormatExtensions.kt
+++ app/src/main/java/com/flinesoft/fitnesstracker/globals/extensions/MeasureFormatExtensions.kt
@@ -1,5 +1,6 @@
 package com.flinesoft.fitnesstracker.globals.extensions
 
+import android.icu.text.DecimalFormat
 import android.icu.text.MeasureFormat
 import android.icu.util.Measure
 import android.icu.util.MeasureUnit
@@ -9,17 +10,17 @@ object MeasureFormatExt {
     val short: MeasureFormat = MeasureFormat.getInstance(ULocale.getDefault(), MeasureFormat.FormatWidth.SHORT)
 }
 
-// TODO: continue here by adding a unit tests for parsing string to int/double for big numbers (separators aren't parsed correctly)
-
 fun MeasureFormat.valueToString(value: Double, unit: MeasureUnit): String = format(Measure(value, unit))
 fun MeasureFormat.valueToString(value: Int, unit: MeasureUnit): String = format(Measure(value, unit))
 
 fun MeasureFormat.stringToDouble(string: String, unit: MeasureUnit): Double? {
-    val valueText: String = string.replace(getUnitDisplayName(unit), "").trim()
+    val groupingSeparator: String = (numberFormat as DecimalFormat).decimalFormatSymbols.groupingSeparatorString
+    val valueText: String = string.replace(getUnitDisplayName(unit), "").replace(groupingSeparator, "").trim()
     return if (valueText.isNotEmpty()) valueText.toDouble() else null
 }
 
 fun MeasureFormat.stringToInt(string: String, unit: MeasureUnit): Int? {
-    val valueText: String = string.replace(getUnitDisplayName(unit), "").trim()
+    val groupingSeparator: String = (numberFormat as DecimalFormat).decimalFormatSymbols.groupingSeparatorString
+    val valueText: String = string.replace(getUnitDisplayName(unit), "").replace(groupingSeparator, "").trim()
     return if (valueText.isNotEmpty()) valueText.toInt() else null
 }
