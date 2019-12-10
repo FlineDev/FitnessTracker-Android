1cbedc4b3ce2d4e6ebaa570fcc8fdba1f232cd5c
Cihat Gündüz <commits@cihatguenduez.de>
2019-12-10 08:23:31 +0100

    Write tests for all persisted models

---
+++ app/src/androidTest/kotlin/com/flinesoft/fitnesstracker/persistence/WeightMeasurementTest.kt
@@ -0,0 +1,36 @@
+package com.flinesoft.fitnesstracker.persistence
+
+import androidx.test.runner.AndroidJUnit4
+import com.flinesoft.fitnesstracker.helpers.extensions.awaitValue
+import com.flinesoft.fitnesstracker.model.WeightMeasurement
+import org.joda.time.DateTime
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class WeightMeasurementTest: PersistenceTest() {
+    @Test
+    @Throws(Exception::class)
+    fun runBasicCRUDOperations() {
+        val allMeasurementsSortedByMeasureDate = database.weightMeasurementDao.allOrderedByMeasureDate()
+        assertNotNull(allMeasurementsSortedByMeasureDate.awaitValue())
+        assert(allMeasurementsSortedByMeasureDate.awaitValue()!!.isEmpty())
+
+        val measurement = database.weightMeasurementDao.create(
+            WeightMeasurement(80.0, DateTime(2019, 12, 18, 15, 20))
+        ).awaitValue()!!
+        assertEquals(measurement.id, allMeasurementsSortedByMeasureDate.awaitValue()!!.first().id)
+        assertEquals(1, allMeasurementsSortedByMeasureDate.awaitValue()!!.size)
+
+        val newWeightInKilograms = 75.5
+        measurement.weightInKilograms = newWeightInKilograms
+        database.weightMeasurementDao.update(measurement)
+        assertEquals(newWeightInKilograms, allMeasurementsSortedByMeasureDate.awaitValue()!!.first().weightInKilograms, 0.01)
+
+        database.weightMeasurementDao.delete(measurement)
+        assert(allMeasurementsSortedByMeasureDate.awaitValue()!!.isEmpty())
+    }
+}
---
+++ app/src/androidTest/kotlin/com/flinesoft/fitnesstracker/persistence/WorkoutTest.kt
@@ -0,0 +1,40 @@
+package com.flinesoft.fitnesstracker.persistence
+
+import androidx.test.runner.AndroidJUnit4
+import com.flinesoft.fitnesstracker.helpers.extensions.awaitValue
+import com.flinesoft.fitnesstracker.model.Workout
+import org.joda.time.DateTime
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class WorkoutTest: PersistenceTest() {
+    @Test
+    @Throws(Exception::class)
+    fun runBasicCRUDOperations() {
+        val allWorkoutsOrderedByStartDate = database.workoutDao.allOrderedByStartDate()
+        assertNotNull(allWorkoutsOrderedByStartDate.awaitValue())
+        assert(allWorkoutsOrderedByStartDate.awaitValue()!!.isEmpty())
+
+        val workout = database.workoutDao.create(
+            Workout(
+                Workout.Type.CARDIO,
+                DateTime(2019, 12, 18, 15, 20),
+                DateTime(2019, 12, 18, 16, 50)
+            )
+        ).awaitValue()!!
+        assertEquals(workout.id, allWorkoutsOrderedByStartDate.awaitValue()!!.first().id)
+        assertEquals(1, allWorkoutsOrderedByStartDate.awaitValue()!!.size)
+
+        val newType = Workout.Type.MUSCLE_BUILDING
+        workout.type = newType
+        database.workoutDao.update(workout)
+        assertEquals(newType, allWorkoutsOrderedByStartDate.awaitValue()!!.first().type)
+
+        database.workoutDao.delete(workout)
+        assert(allWorkoutsOrderedByStartDate.awaitValue()!!.isEmpty())
+    }
+}
