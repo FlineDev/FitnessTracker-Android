59d7cbfcd09bcdb75283e736a612996d3063416a
Cihat Gündüz <commits@cihatguenduez.de>
2020-01-26 10:10:24 +0100

    Improve string resource safety

--- app/src/main/kotlin/com/flinesoft/fitnesstracker/globals/extensions/ViewExt.kt
+++ app/src/main/kotlin/com/flinesoft/fitnesstracker/globals/extensions/ViewExt.kt
@@ -2,9 +2,10 @@ package com.flinesoft.fitnesstracker.globals.extensions
 
 import android.view.View
 import android.view.ViewGroup.MarginLayoutParams
+import androidx.annotation.StringRes
 import com.google.android.material.snackbar.Snackbar
 
-fun View.snack(resId: Int, duration: Int = Snackbar.LENGTH_SHORT) {
+fun View.snack(@StringRes resId: Int, duration: Int = Snackbar.LENGTH_SHORT) {
     Snackbar.make(this, resId, duration).apply {
         (view.layoutParams as? MarginLayoutParams)?.apply {
             // TODO: changing bottom margin doesn't work, left & right are working though ...
