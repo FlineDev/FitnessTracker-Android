af49e0d80a62a336a998662a3feccf1073894e76
Cihat Gündüz <commits@cihatguenduez.de>
2020-02-09 11:13:02 +0100

    Re-enable CleanStatusBar & refactor test code

--- app/src/androidTest/kotlin/com/flinesoft/fitnesstracker/OnboardingTest.kt
+++ app/src/androidTest/kotlin/com/flinesoft/fitnesstracker/OnboardingTest.kt
@@ -2,44 +2,17 @@ package com.flinesoft.fitnesstracker
 
 
 import androidx.test.filters.LargeTest
-import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
 import com.flinesoft.fitnesstracker.helpers.EspressoTest
-import com.flinesoft.fitnesstracker.helpers.extensions.TestContext
-import org.junit.*
+import org.junit.Test
 import org.junit.runner.RunWith
 import tools.fastlane.screengrab.Screengrab.screenshot
-import tools.fastlane.screengrab.locale.LocaleTestRule
 import kotlin.time.ExperimentalTime
 
 @ExperimentalTime
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 class OnboardingTest: EspressoTest() {
-    companion object {
-        @ClassRule
-        @JvmField
-        public val localeTestRule = LocaleTestRule()
-    }
-
-    @Before
-    fun setUp() {
-        TestContext.resetAll()
-        // TODO: [2020-02-08] waiting for https://github.com/fastlane/fastlane/issues/15777 being fixed
-//        CleanStatusBar.enableWithDefaults()
-    }
-
-    @After
-    fun tearDown() {
-        // TODO: [2020-02-08] waiting for https://github.com/fastlane/fastlane/issues/15777 being fixed
-//        CleanStatusBar.disable()
-        TestContext.resetAll()
-    }
-
-    @Rule
-    @JvmField
-    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
-
     @Test
     fun onboardingTest() {
         checkTextIsFullyVisible(R.string.onboarding_workouts_title)
--- app/src/androidTest/kotlin/com/flinesoft/fitnesstracker/helpers/EspressoTest.kt
+++ app/src/androidTest/kotlin/com/flinesoft/fitnesstracker/helpers/EspressoTest.kt
@@ -7,9 +7,42 @@ import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.rule.ActivityTestRule
+import com.flinesoft.fitnesstracker.MainActivity
+import com.flinesoft.fitnesstracker.helpers.extensions.TestContext
 import org.hamcrest.Matchers.allOf
+import org.junit.After
+import org.junit.Before
+import org.junit.ClassRule
+import org.junit.Rule
+import tools.fastlane.screengrab.cleanstatusbar.CleanStatusBar
+import tools.fastlane.screengrab.locale.LocaleTestRule
+import kotlin.time.ExperimentalTime
 
+@ExperimentalTime
 open class EspressoTest {
+    companion object {
+        @ClassRule
+        @JvmField
+        public val localeTestRule = LocaleTestRule()
+    }
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Before
+    open fun setUp() {
+        TestContext.resetAll()
+        CleanStatusBar.enableWithDefaults()
+    }
+
+    @After
+    open fun tearDown() {
+        CleanStatusBar.disable()
+        TestContext.resetAll()
+    }
+
     fun clickOnFullyVisibleView(@IdRes id: Int): ViewInteraction = onView(allOf(withId(id), isCompletelyDisplayed())).perform(click())
     fun checkViewIsFullyVisible(@IdRes id: Int): ViewInteraction = onView(allOf(withId(id))).check(matches(isCompletelyDisplayed()))
     fun checkTextIsFullyVisible(@StringRes id: Int): ViewInteraction = onView(allOf(withText(id))).check(matches(isCompletelyDisplayed()))
