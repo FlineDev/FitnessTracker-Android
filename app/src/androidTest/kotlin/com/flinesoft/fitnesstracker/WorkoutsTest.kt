d9fdecd2e66ff776dfcf30cce52d90c3513f2cf4
Cihat Gündüz <commits@cihatguenduez.de>
2020-02-13 00:19:21 +0100

    Extend workouts UI tests + Update screenshots

--- app/src/androidTest/kotlin/com/flinesoft/fitnesstracker/WorkoutsTest.kt
+++ app/src/androidTest/kotlin/com/flinesoft/fitnesstracker/WorkoutsTest.kt
@@ -2,6 +2,7 @@ package com.flinesoft.fitnesstracker
 
 import com.flinesoft.fitnesstracker.helpers.EspressoTest
 import com.flinesoft.fitnesstracker.helpers.extensions.TestContext
+import org.joda.time.DateTime
 import org.junit.Test
 import tools.fastlane.screengrab.Screengrab.screenshot
 import kotlin.time.ExperimentalTime
@@ -25,12 +26,12 @@ class WorkoutsTest: EspressoTest() {
         checkStringsAreFullyVisible(R.string.workouts_speed_dial_workout)
         screenshot("Workouts_EmptyHistory_OpenSpeedDial")
 
-        clickOnFullyVisibleText(R.string.workouts_speed_dial_workout)
+        clickOnFullyVisibleString(R.string.workouts_speed_dial_workout)
         checkStringsAreFullyVisible(R.string.workouts_edit_workout_title_new)
         checkViewsAreFullyVisible(R.id.workoutTypeSpinner, R.id.startDateEditText, R.id.startTimeEditText, R.id.startDateEditText, R.id.startTimeEditText)
         screenshot("Workouts_NewWorkout")
 
-        clickOnFullyVisibleText(R.string.global_action_save)
+        clickOnFullyVisibleString(R.string.global_action_save)
         checkStringsAreFullyVisible(R.string.workouts_header_title)
         checkStringsDoNotExist(R.string.workouts_history_empty_state)
         checkViewsDoNotExist(R.id.impedimentEntry)
@@ -38,24 +39,74 @@ class WorkoutsTest: EspressoTest() {
         screenshot("Workouts_FirstEntryHistory")
 
         clickOnFullyVisibleView(R.id.workoutsSpeedDial)
-        clickOnFullyVisibleText(R.string.workouts_speed_dial_impediment)
+        clickOnFullyVisibleString(R.string.workouts_speed_dial_impediment)
         checkStringsAreFullyVisible(R.string.workouts_edit_impediment_title_new)
         checkViewsAreFullyVisible(R.id.nameEditText, R.id.startDateEditText, R.id.endDateEditText)
         screenshot("Workouts_NewImpediment")
 
-        clickOnFullyVisibleText(R.string.global_action_save)
+        clickOnFullyVisibleString(R.string.global_action_save)
         checkStringsAreFullyVisible(R.string.global_error_invalid_input, R.string.workouts_edit_impediment_title_new)
         screenshot("Workouts_NewImpediment_InvalidInput")
         Thread.sleep(3_000)
 
         typeTextInFullyVisibleView("Cold", R.id.nameEditText)
-        clickOnFullyVisibleText(R.string.global_action_save)
+        clickOnFullyVisibleString(R.string.global_action_save)
         checkTextsAreFullyVisible("Cold")
         checkStringsAreFullyVisible(R.string.workouts_header_title)
         checkStringsDoNotExist(R.string.workouts_history_empty_state)
         checkViewsAreFullyVisible(R.id.nextWorkoutDateTextView, R.id.workoutsSpeedDial, R.id.workoutEntry, R.id.impedimentEntry)
         screenshot("Workouts_TwoEntriesHistory")
 
-        // TODO: [2020-02-09] also test editing of workout, impediment and reminder settings
+        clickOnFullyVisibleString(R.string.models_workout_type_cardio)
+        checkStringsAreFullyVisible(R.string.workouts_edit_workout_title_edit, R.string.global_action_cancel, R.string.global_action_save)
+        checkStringsDoNotExist(R.string.workouts_header_title)
+        screenshot("Workouts_EditWorkout")
+
+        clickOnFullyVisibleView(R.id.workoutTypeSpinner)
+        clickOnFullyVisibleString(R.string.models_workout_type_muscle_building)
+        clickOnFullyVisibleView(R.id.endTimeEditText)
+        screenshot("Workouts_EditWorkout_EndTime")
+
+        val tenMinutesAgo = DateTime.now().minusMinutes(10)
+        chooseTimeInFullyVisibleTimePicker(tenMinutesAgo.hourOfDay, tenMinutesAgo.minuteOfHour)
+        clickOnFullyVisibleView(android.R.id.button1) // OK button
+        clickOnFullyVisibleString(R.string.global_action_save)
+        checkStringsDoNotExist(R.string.workouts_edit_workout_title_edit, R.string.global_action_cancel, R.string.global_action_save)
+        checkStringsAreFullyVisible(R.string.workouts_header_title, R.string.models_workout_type_muscle_building)
+        screenshot("Workouts_TwoEntriesHistory_AfterEditWorkout")
+
+        clickOnFullyVisibleText("Cold")
+        checkStringsAreFullyVisible(R.string.workouts_edit_impediment_title_edit, R.string.global_action_cancel, R.string.global_action_save)
+        checkStringsDoNotExist(R.string.workouts_header_title)
+        screenshot("Workouts_EditImpediment")
+
+        typeTextInFullyVisibleView("Flu", R.id.nameEditText)
+        clickOnFullyVisibleView(R.id.endDateEditText)
+        screenshot("Workouts_EditImpediment_EndTime")
+
+        val aWeekFromNow = DateTime.now().plusDays(7)
+        chooseDateInFullyVisibleDatePicker(aWeekFromNow.year, aWeekFromNow.monthOfYear, aWeekFromNow.dayOfMonth)
+        clickOnFullyVisibleView(android.R.id.button1) // OK button
+        clickOnFullyVisibleString(R.string.global_action_save)
+        checkStringsDoNotExist(R.string.workouts_edit_impediment_title_edit, R.string.global_action_cancel, R.string.global_action_save)
+        checkStringsAreFullyVisible(R.string.workouts_header_title)
+        checkTextsAreFullyVisible("Flu")
+        screenshot("Workouts_TwoEntriesHistory_AfterEditImpediment")
+
+        openActionBarOverflowOrOptionsMenu()
+        screenshot("Workouts_OverflowMenuOpened")
+
+        checkStringsAreFullyVisible(R.string.workouts_overflow_reminder, R.string.shared_overflow_feedback)
+        clickOnFullyVisibleString(R.string.workouts_overflow_reminder)
+        checkEditTextsAreEnabled(R.id.reminderTimeEditText)
+        screenshot("Workouts_ConfigureReminders")
+
+        clickOnFullyVisibleString(R.string.workouts_edit_reminders_reminder_on_title)
+        checkEditTextsAreDisabled(R.id.reminderTimeEditText)
+        screenshot("Workouts_ConfigureReminders_TurnedOff")
+
+        clickOnFullyVisibleString(R.string.global_action_save)
+        checkStringsDoNotExist(R.string.workouts_overflow_reminder, R.string.shared_overflow_feedback, R.string.workouts_edit_reminders_reminder_on_title)
+        checkStringsAreFullyVisible(R.string.workouts_header_title)
     }
 }
--- app/src/androidTest/kotlin/com/flinesoft/fitnesstracker/helpers/EspressoTest.kt
+++ app/src/androidTest/kotlin/com/flinesoft/fitnesstracker/helpers/EspressoTest.kt
@@ -1,18 +1,22 @@
 package com.flinesoft.fitnesstracker.helpers
 
+import android.widget.DatePicker
+import android.widget.TimePicker
 import androidx.annotation.IdRes
 import androidx.annotation.StringRes
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.PickerActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.espresso.util.HumanReadables
 import androidx.test.rule.ActivityTestRule
 import com.flinesoft.fitnesstracker.MainActivity
 import com.flinesoft.fitnesstracker.helpers.extensions.TestContext
-import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.*
 import org.junit.After
 import org.junit.Before
 import org.junit.ClassRule
@@ -62,11 +66,25 @@ open class EspressoTest {
     }
 
     fun clickOnFullyVisibleView(@IdRes id: Int): ViewInteraction = onView(allOf(withId(id), isCompletelyDisplayed())).perform(click())
-    fun clickOnFullyVisibleText(@StringRes id: Int): ViewInteraction = onView(allOf(withText(id), isCompletelyDisplayed())).perform(click())
+    fun clickOnFullyVisibleString(@StringRes id: Int): ViewInteraction = onView(allOf(withText(id), isCompletelyDisplayed())).perform(click())
+    fun clickOnFullyVisibleText(text: String): ViewInteraction = onView(allOf(withText(text), isCompletelyDisplayed())).perform(click())
+    fun openActionBarOverflowOrOptionsMenu() = openActionBarOverflowOrOptionsMenu(mainActivityTestRule.activity.applicationContext)
 
     fun typeTextInFullyVisibleView(text: String, @IdRes id: Int): ViewInteraction = onView(
         allOf(withId(id), isCompletelyDisplayed())
     ).perform(scrollTo(), click(), clearText(), typeText(text), closeSoftKeyboard())
+
+    fun chooseDateInFullyVisibleDatePicker(year: Int, monthOfYear: Int, dayOfMonth: Int): ViewInteraction = onView(
+        withClassName(equalTo(DatePicker::class.java.name))
+    ).check(
+        matches(isCompletelyDisplayed())
+    ).perform(PickerActions.setDate(year, monthOfYear, dayOfMonth))
+
+    fun chooseTimeInFullyVisibleTimePicker(hours: Int, minutes: Int): ViewInteraction = onView(
+        withClassName(equalTo(TimePicker::class.java.name))
+    ).check(
+        matches(isCompletelyDisplayed())
+    ).perform(PickerActions.setTime(hours, minutes))
 
     fun checkViewsDoNotExist(@IdRes vararg ids: Int) = ids.forEach { onView(withId(it)).check(isNotDisplayed()) }
     fun checkStringsDoNotExist(@IdRes vararg ids: Int) = ids.forEach { onView(withText(it)).check(isNotDisplayed()) }
@@ -75,6 +93,9 @@ open class EspressoTest {
     fun checkViewsAreFullyVisible(@IdRes vararg ids: Int) = ids.forEach { onView(withId(it)).check(matches(isCompletelyDisplayed())) }
     fun checkStringsAreFullyVisible(@IdRes vararg ids: Int) = ids.forEach { onView(withText(it)).check(matches(isCompletelyDisplayed())) }
     fun checkTextsAreFullyVisible(vararg texts: String) = texts.forEach { onView(withText(it)).check(matches(isCompletelyDisplayed())) }
+
+    fun checkEditTextsAreEnabled(@IdRes vararg ids: Int) = ids.forEach { onView(withId(it)).check(matches(isEnabled())) }
+    fun checkEditTextsAreDisabled(@IdRes vararg ids: Int) = ids.forEach { onView(withId(it)).check(matches(not(isEnabled()))) }
 
     private fun isNotDisplayed(): ViewAssertion? {
         return ViewAssertion { view, _ ->
