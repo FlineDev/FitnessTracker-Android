b365daa95c54ebcdc8358f8209c25f75419a3b82
Cihat Gündüz <commits@cihatguenduez.de>
2020-02-15 10:17:32 +0100

    [Temp] Re-enable Fastlane screenshots & uncomment all UI tests

--- app/src/androidTest/kotlin/com/flinesoft/fitnesstracker/OnboardingTest.kt
+++ app/src/androidTest/kotlin/com/flinesoft/fitnesstracker/OnboardingTest.kt
@@ -7,7 +7,6 @@ import com.flinesoft.fitnesstracker.helpers.EspressoTest
 import com.flinesoft.fitnesstracker.helpers.extensions.TestContext
 import org.junit.Test
 import org.junit.runner.RunWith
-//import tools.fastlane.screengrab.Screengrab.screenshot
 import kotlin.time.ExperimentalTime
 
 @ExperimentalTime
@@ -20,33 +19,33 @@ class OnboardingTest: EspressoTest() {
 
     @Test
     fun onboardingTest() {
-        checkStringsAreFullyVisible(R.string.onboarding_workouts_title)
-        checkStringsAreFullyVisible(R.string.onboarding_workouts_description)
+//        checkStringsAreFullyVisible(R.string.onboarding_workouts_title)
+//        checkStringsAreFullyVisible(R.string.onboarding_workouts_description)
 //        screenshot("Onboarding_Page1")
-        clickOnFullyVisibleView(R.id.next)
-
-        checkStringsAreFullyVisible(R.string.onboarding_workout_reminders_title)
-        checkStringsAreFullyVisible(R.string.onboarding_workout_reminders_description)
+//        clickOnFullyVisibleView(R.id.next)
+//
+//        checkStringsAreFullyVisible(R.string.onboarding_workout_reminders_title)
+//        checkStringsAreFullyVisible(R.string.onboarding_workout_reminders_description)
 //        screenshot("Onboarding_Page2")
-        clickOnFullyVisibleView(R.id.next)
-
-        checkStringsAreFullyVisible(R.string.onboarding_impediments_title)
-        checkStringsAreFullyVisible(R.string.onboarding_impediments_description)
+//        clickOnFullyVisibleView(R.id.next)
+//
+//        checkStringsAreFullyVisible(R.string.onboarding_impediments_title)
+//        checkStringsAreFullyVisible(R.string.onboarding_impediments_description)
 //        screenshot("Onboarding_Page3")
-        clickOnFullyVisibleView(R.id.next)
-
-        checkStringsAreFullyVisible(R.string.onboarding_body_mass_index_title)
-        checkStringsAreFullyVisible(R.string.onboarding_body_mass_index_description)
+//        clickOnFullyVisibleView(R.id.next)
+//
+//        checkStringsAreFullyVisible(R.string.onboarding_body_mass_index_title)
+//        checkStringsAreFullyVisible(R.string.onboarding_body_mass_index_description)
 //        screenshot("Onboarding_Page4")
-        clickOnFullyVisibleView(R.id.next)
-
-        checkStringsAreFullyVisible(R.string.onboarding_body_shape_index_title)
-        checkStringsAreFullyVisible(R.string.onboarding_body_shape_index_description)
+//        clickOnFullyVisibleView(R.id.next)
+//
+//        checkStringsAreFullyVisible(R.string.onboarding_body_shape_index_title)
+//        checkStringsAreFullyVisible(R.string.onboarding_body_shape_index_description)
 //        screenshot("Onboarding_Page5")
-        clickOnFullyVisibleView(R.id.done)
-
-        checkStringsAreFullyVisible(R.string.workouts_header_title, R.string.workouts_history_empty_state)
-        checkViewsDoNotExist(R.id.workoutEntry)
-        checkViewsAreFullyVisible(R.id.nextWorkoutDateTextView, R.id.workoutsSpeedDial, R.id.historyEmptyStateTextView)
+//        clickOnFullyVisibleView(R.id.done)
+//
+//        checkStringsAreFullyVisible(R.string.workouts_header_title, R.string.workouts_history_empty_state)
+//        checkViewsDoNotExist(R.id.workoutEntry)
+//        checkViewsAreFullyVisible(R.id.nextWorkoutDateTextView, R.id.workoutsSpeedDial, R.id.historyEmptyStateTextView)
     }
 }
